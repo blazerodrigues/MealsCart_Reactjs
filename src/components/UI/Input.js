1d60ebdc711d42505f47bfb2d0064737c95873e1
Blaze Rodrigues <blazerodrigues@gmail.com>
2022-04-10 17:58:27 +0530

    Initial commit

---
+++ src/components/UI/Input.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './Input.module.css';
+
+const Input = React.forwardRef((props, ref) => {
+
+    return(
+        <div className={classes.input}>
+
+            <label>
+                <label htmlFor={props.input.id}>{props.label}</label>
+            </label>
+
+            <input ref={ref} {...props.input} />
+
+        </div>
+    )
+
+});
+
+export default Input;
